cd363d7f9f1c00ca8c0b853d5566b8ec0ff152bd
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2019-02-01 16:58:56 +0900

    Add replier

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 app.use(express.json());
+
 const PORT = process.env.PORT
+const BOT_TOKEN = process.env.BOT_TOKEN
+
 var server = app.listen(PORT, function() {
   console.log("Server started on port " + PORT);
 })
 
 app.post('/', function(req, res) {
-    console.log("REQUEST> " + req.body);
+    console.log("REQUEST> ");
+    console.log(req.body);
     let payload = req.body;
 
     if (payload.type === "url_verification") {
@@ -16,6 +21,24 @@ app.post('/', function(req, res) {
       res.sendStatus(200);
       if (payload.event.type === "app_mention") {
         console.log(payload.event.text);
+
+        let payload_reply = {
+          'token': BOT_TOKEN,
+          'text': 'Knock, knock.',
+          'channel': payload.event.channel };
+
+        request.post(
+            { 
+              headers: { 'content-type' : 'application/json' },
+              url: "https://slack.com/api/chat.postMessage",
+              body: JSON.stringify(payload_reply),
+            },
+            function (error, response, body) {
+              console.log("POST request sent> ")
+              console.log(error);
+              console.log(body); 
+            }
+        ); 
       }
     }
 })
